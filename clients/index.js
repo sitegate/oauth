d948977beeceb180e9c2d87e47f4bc7a6e59091e
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-10 23:48:20 +0200

    microservice methods explicitly listed

--- clients/index.js
+++ clients/index.js
@@ -1,5 +1,4 @@
 'use strict'
-
 module.exports = function(service, opts, next) {
   if (!opts.amqpURL)
     return next(new Error('amqpURL is required'))
@@ -8,14 +7,42 @@ module.exports = function(service, opts, next) {
     name: 'user',
     channel: 'sitegate-user',
     url: opts.amqpURL,
-    methods: [],
+    methods: [
+      'authenticate',
+      'changePassword',
+      'changePasswordUsingToken',
+      'disconnectProvider',
+      'getById',
+      'getByUsername',
+      'getTrustedClients',
+      'query',
+      'register',
+      'requestPasswordChangeByEmail',
+      'revokeAllClientsAccess',
+      'revokeClientAccess',
+      'saveOAuthUserProfile',
+      'sendVerificationEmail',
+      'trustClient',
+      'trustsClient',
+      'update',
+      'validateResetToken',
+      'verifyEmailByToken',
+    ],
   })
 
   service.client({
     name: 'client',
     channel: 'sitegate-client',
     url: opts.amqpURL,
-    methods: [],
+    methods: [
+      'create',
+      'getById',
+      'getById',
+      'getByPublicId',
+      'query',
+      'remove',
+      'update',
+    ],
   })
 
   next()
